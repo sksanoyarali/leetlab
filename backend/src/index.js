5a82214a94261021c7c2a93569d87c021f6de58f
SK sanoyar ali <asksanoyar33@gmail.com>
2025-09-22 21:03:46 +0530

    prisma with docker setup done

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+app.get('/', (req, res) => {
+  res.send('main route')
+})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
