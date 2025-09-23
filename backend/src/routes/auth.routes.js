df2fa53deb77dde6ff70afd4eb721ffc8174b2f5
SK sanoyar ali <asksanoyar33@gmail.com>
2025-09-23 20:55:19 +0530

    authetication route setup done

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express, { Router } from 'express'
+import {
+  check,
+  login,
+  logout,
+  register,
+} from '../controller/auth.controller.js'
+const authRoutes = Router()
+
+authRoutes.post('/register', register)
+authRoutes.post('/logout', login)
+authRoutes.get('/logout', logout)
+authRoutes.get('/check', check)
+export default authRoutes
