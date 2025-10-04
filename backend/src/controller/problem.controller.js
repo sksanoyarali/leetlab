ad258bd8075106a7539a176599e8fbbe4e573ad6
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-05 00:21:17 +0530

    problem routes set up done

---
+++ backend/src/controller/problem.controller.js
@@ -0,0 +1,20 @@
+const createProblem = async (req, res) => {}
+
+const getAllProblems = async (req, res) => {}
+
+const getProblemById = async (req, res) => {}
+
+const getAllproblemsSolvedByUser = async (req, res) => {}
+
+const updateProblem = async (req, res) => {}
+
+const deleteProblem = async (req, res) => {}
+
+export {
+  createProblem,
+  getAllProblems,
+  getAllproblemsSolvedByUser,
+  getProblemById,
+  updateProblem,
+  deleteProblem,
+}
--- backend/src/routes/problem.routes.js
+++ backend/src/routes/problem.routes.js
@@ -1,3 +1,35 @@
 import express from 'express'
+import { authMiddleware, checkAdmin } from '../middlewares/auth.middleware.js'
+import {
+  createProblem,
+  deleteProblem,
+  getAllproblemsSolvedByUser,
+  getProblemById,
+  updateProblem,
+} from '../controller/problem.controller.js'
 const problemRoutes = express.Router()
+problemRoutes.post('/create-problem', authMiddleware, checkAdmin, createProblem)
+problemRoutes.get(
+  '/get-all-problems',
+  authMiddleware,
+  getAllproblemsSolvedByUser
+)
+problemRoutes.get('/get-problem/:id', authMiddleware, getProblemById)
+problemRoutes.put(
+  '/update-problem/:id',
+  authMiddleware,
+  checkAdmin,
+  updateProblem
+)
+problemRoutes.delete(
+  '/delete-problem/:id',
+  authMiddleware,
+  checkAdmin,
+  deleteProblem
+)
+problemRoutes.get(
+  'get-solved-problems',
+  authMiddleware,
+  getAllproblemsSolvedByUser
+)
 export default problemRoutes
