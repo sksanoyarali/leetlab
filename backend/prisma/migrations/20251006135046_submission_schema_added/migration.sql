b82dcdaa53dcac6f7a3a89c301c7d1b9cf767bc7
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-06 19:22:12 +0530

    Add Submission model and migration

---
+++ backend/prisma/migrations/20251006135046_submission_schema_added/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "public"."Submission" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "sourceCode" JSONB NOT NULL,
+    "language" TEXT NOT NULL,
+    "stdin" TEXT,
+    "stdout" TEXT,
+    "stderr" TEXT NOT NULL,
+    "compileOuput" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "memory" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Submission" ADD CONSTRAINT "Submission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Submission" ADD CONSTRAINT "Submission_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "public"."Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/src/controller/problem.controller.js
+++ backend/src/controller/problem.controller.js
@@ -128,12 +128,128 @@ const getProblemById = async (req, res) => {
     })
   }
 }
+const updateProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body
+
+  if (req.user.role !== 'ADMIN') {
+    return res.status(403).json({
+      error: 'You are not allowed to create a problem',
+    })
+  }
+  try {
+    const { id } = req.params
+    const problem = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    })
+    if (!problem) {
+      return res.status(404).json({
+        error: 'Problem does not exist',
+      })
+    }
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language)
+      if (!languageId) {
+        return res.status(400).json({
+          error: `language ${language} is not supported`,
+        })
+      }
+
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }))
+
+      const submissionResults = await submitBatch(submissions)
+
+      const tokens = submissionResults.map((res) => res.token)
+
+      const results = await pollBatchResults(tokens)
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i]
+        console.log('result', result)
+
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `testcase ${i + 1} failed for language ${language}`,
+          })
+        }
+      }
+    }
+    const updatedProblem = await db.problem.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user.id,
+      },
+    })
+    return res.status(201).json({
+      message: 'problem created successfully',
+      success: true,
+      problem: updatedProblem,
+    })
+  } catch (error) {
+    console.log(error)
+
+    return res.status(500).json({
+      error: 'Error in Updating creation',
+    })
+  }
+}
+
+const deleteProblem = async (req, res) => {
+  const { id } = req.params
+  try {
+    const problem = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    })
+    if (!problem) {
+      return res.status(404).json({
+        error: 'Problem does not exist',
+      })
+    }
+    await db.problem.delete({
+      where: {
+        id,
+      },
+    })
+    res.status(200).json({
+      success: true,
+      message: 'problem deleted successfully',
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Error in Deleting Problem',
+    })
+  }
+}
 
 const getAllproblemsSolvedByUser = async (req, res) => {}
-
-const updateProblem = async (req, res) => {}
-
-const deleteProblem = async (req, res) => {}
 
 export {
   createProblem,
