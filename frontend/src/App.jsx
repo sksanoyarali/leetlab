4ae6c9775f42f349bcf262f3941ad926eaba5cc3
SK sanoyar ali <asksanoyar33@gmail.com>
2026-02-20 00:08:30 +0530

    added auth and addproblem  frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import HomePage from './page/HomePage'
+import LoginPage from './page/LoginPage'
+import SignUpPage from './page/SignUpPage'
+import { useAuthStore } from './store/useAuthStore'
+import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
+import Layout from './layout/Layout'
+import AdminRoute from './components/AdminRoute'
+import AddProblem from './page/AddProblem'
 
 const App = () => {
-  return <div className="">App</div>
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Toaster />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={'/login'} />}
+          />
+        </Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={'/'} />}
+        />
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />}
+          ></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
